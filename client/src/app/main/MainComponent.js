c82da8e32f42f1115de31a351a6a743a6a61979d
Javan Poirier <Javan.Poirier@Outlook.com>
2018-05-28 22:15:43 -0300

    test

---
+++ client/src/app/main/MainComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import './style.css';
+import { Provider } from 'react-redux';
+
+
+function MainComponent({
+
+}) {
+    return {
+        
+        
+    }
+}
